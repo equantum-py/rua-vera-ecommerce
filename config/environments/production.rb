29e7fdb468e80f81c4d4c734f72b1916705f58d0
eCommerce proyectos <equantumg@gmail.com>
2026-09-21 08:42:12 -0300

    feat: build Rails storefront foundation

---
+++ config/environments/production.rb
@@ -0,0 +1,10 @@
+require "active_support/core_ext/integer/time"
+Rails.application.configure do
+  config.enable_reloading = false
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
+  config.active_storage.service = :local
+  config.force_ssl = ENV.fetch("FORCE_SSL", "true") == "true"
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+end
