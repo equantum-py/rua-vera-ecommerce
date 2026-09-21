de965f9d6eb8e4f24129a37ca3c7f2f8c252765b
eCommerce proyectos <equantumg@gmail.com>
2026-09-21 08:57:59 -0300

    feat: add Rails admin catalog CRUD

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,25 @@
+module Admin
+  class OrdersController < ApplicationController
+    before_action :set_order, only: %i[show update]
+
+    def index
+      @orders = Order.order(created_at: :desc)
+    end
+    def show; end
+    def update
+      if @order.update(order_params)
+        redirect_to admin_order_path(@order), notice: "Pedido actualizado."
+      else
+        render :show, status: :unprocessable_entity
+      end
+    end
+
+    private
+    def set_order
+      @order = Order.find(params[:id])
+    end
+    def order_params
+      params.require(:order).permit(:status, :payment_status, :erp_reference)
+    end
+  end
+end
