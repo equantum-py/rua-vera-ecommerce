905c60164921b5e07c64ee23900670fd8100b692
eCommerce proyectos <equantumg@gmail.com>
2026-09-21 08:37:01 -0300

    feat: start Ruby on Rails ecommerce migration

---
+++ app/controllers/storefront_controller.rb
@@ -0,0 +1,16 @@
+class StorefrontController < ApplicationController
+  def home
+    @categories = Category.includes(:products).all
+    @featured_products = Product.where(active: true).order(created_at: :desc).limit(16)
+  end
+
+  def shop
+    @categories = Category.all
+    @products = Product.where(active: true)
+    @products = @products.joins(:category).where(categories: { name: params[:category] }) if params[:category].present?
+  end
+
+  def product
+    @product = Product.find(params[:id])
+  end
+end
