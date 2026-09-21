c6ba73efdb903f05ec85d4a50d423015e674c322
eCommerce proyectos <equantumg@gmail.com>
2026-09-21 10:38:40 -0300

    feat: add inventory promotions banners and integrations foundation

---
+++ app/controllers/admin/inventory_controller.rb
@@ -0,0 +1,14 @@
+module Admin
+  class InventoryController < ApplicationController
+    def index
+      @products = Product.includes(:category).order(:stock, :name)
+      @low_stock = @products.select { |product| product.stock <= 5 }
+    end
+
+    def update
+      product = Product.find(params[:id])
+      product.update!(stock: [params[:stock].to_i, 0].max)
+      redirect_to admin_inventory_index_path, notice: "Stock actualizado."
+    end
+  end
+end
