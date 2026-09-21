c976549f80ec0499c23bd82429ad9aeae77ae7d8
eCommerce proyectos <equantumg@gmail.com>
2026-09-21 11:10:09 -0300

    security: protect admin controllers with authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 module Admin
-  class DashboardController < ApplicationController
+  class DashboardController < BaseController
     def index
       @products_count = Product.count
       @categories_count = Category.count
